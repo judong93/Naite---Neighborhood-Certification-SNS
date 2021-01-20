0b45df0098075794d4f57704488a945d9f1dbab0
조성훈 <sung95419@gmail.com>
2021-01-20 15:07:05 +0900

    프로젝트: basic spring boot project

---
+++ backend/naite-backend/src/test/java/com/ssafy/naite/NaiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.naite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
