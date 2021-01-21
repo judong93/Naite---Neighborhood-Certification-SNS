ca80a9960fef431bd6e1673e4e452b5d83442a48
이동희 <ldh297@naver.com>
2021-01-21 11:24:55 +0900

    프로젝트: 시작화면 #S04P12A402-20

--- frontend/naite-frontend/src/App.vue
+++ frontend/naite-frontend/src/App.vue
@@ -1,38 +1,62 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+      <router-link to="/about">About</router-link> -->
+    <!-- </div> -->
+    <span class="firstpage-welcome-text">자취생들을 위한 커뮤니티 어서옵쇼라잇</span>
+    <img src="./assets/firstpage.jpg" alt="">
+    <button>START</button>
     <router-view/>
   </div>
 </template>
 
+<script>
+</script>
+
+
 <style>
-
-@local {
-  font-color: red;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
 #nav a.router-link-exact-active {
   /* color: #42b983; */
   color: font-color
 }
+
+.firstpage-welcome-text {
+  position: absolute;
+  top: 100px;
+  left:50%;
+  height:100px;
+  color: black;
+  z-index:1;
+
+}
+
+
+#app > img {
+  position:absolute;
+  top:0;
+  left:0;
+  height: 100vh;
+  width:100vw;
+}
+
+#app > button {
+  position:absolute;
+  background-color: orange;
+  top:50%;
+  left:50%;
+  transform: translate(-50%,-50%)
+}
+
+
 </style>
