be392a3378f0da154fe50dade2ebfa0a0f6fbaf6
이동희 <ldh297@naver.com>
2021-01-22 15:18:05 +0900

    프로젝트: 네브바 컴포넌트 #S04P12A402-33

---
+++ frontend/naite-frontend/src/views/Main.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="main">
+        <Navbar />
+    </div>
+    
+</template>
+<script>
+import Navbar from '@/components/Navbar'
+
+
+export default {
+    name:'Main',
+    components:{
+        Navbar
+                
+    },
+    data: function() {
+        return {
+        }
+    },
+    methods:{
+    },
+    computed: {
+
+    },
+    
+    
+    created:{
+        
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
