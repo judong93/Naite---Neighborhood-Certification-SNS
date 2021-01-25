c28e32bc222b4326b3ef7bca91f60bc920788e0d
kimmk0924 <kimmk0924@gmail.com>
2021-01-25 15:35:45 +0900

    프로젝트: 인텔리제이 구조로 변경

--- backend/naite-backend/src/main/java/com/ssafy/naite/model/mapper/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.naite.model.mapper;
-
-import java.util.List;
-
-import com.ssafy.naite.model.dto.Test;
-
-public interface TestMapper {
-	public List<Test> getAll() throws Exception;
-}
