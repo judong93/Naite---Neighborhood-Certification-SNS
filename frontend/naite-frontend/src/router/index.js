21993fcafff352c721f7740ff11259a08e2a17c3
judong <judong93@naver.com>
2021-01-26 16:49:28 +0900

    기능추가: 장터게시판 기본UI 완성 #S04P12A402-40

--- frontend/naite-frontend/src/router/index.js
+++ frontend/naite-frontend/src/router/index.js
@@ -3,12 +3,9 @@ import VueRouter from 'vue-router'
 import Firstpage from '@/views/Firstpage'
 import Sign from '@/views/Sign'
 import Main from '@/views/Main'
-
-
-
-
+import Profile from '@/views/Profile'
+import GroupBuying from '@/views/GroupBuying'
 Vue.use(VueRouter)
-
 const routes = [
   {
     path: '/',
@@ -19,21 +16,26 @@ const routes = [
     path:'/sign',
     name: 'Sign',
     component: Sign,
-
   },
   {
     path:'/main',
     name: 'Main',
     component: Main,
   },
-
-
+  {
+    path:'/profile',
+    name: 'Profile',
+    component: Profile,
+  },
+  {
+    path:'/groupbuying',
+    name: 'GroupBuying',
+    component: GroupBuying,
+  }
 ]
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
-
 export default router
