0021890cceaae895e7fb7305e6afa7f049b1f2c4
조성훈 <sung95419@gmail.com>
2021-01-26 23:40:23 +0900

    기능추가: 기본 게시판 CRUD #S04P12A402-37

--- backend/naite-backend/src/main/java/com/ssafy/naite/Controller/BoardController.java
+++ backend/naite-backend/src/main/java/com/ssafy/naite/Controller/BoardController.java
@@ -1,7 +1,68 @@
 package com.ssafy.naite.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.naite.dto.board.BoardDto;
+import com.ssafy.naite.service.Board.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
+@RestController("/board")
+@AllArgsConstructor
+@Api(value = "게시판")
 public class BoardController {
+
+    private final BoardService boardService;
+
+    /** 게시글 전체 조회 */
+    @GetMapping("/list")
+    @ApiOperation(value = "게시글 전체 조회")
+    public ResponseEntity<List<BoardDto.BoardResponseDto>> findAllBoards(){
+        List<BoardDto.BoardResponseDto> boardResponseDtoList = boardService.findAllBoards();
+        return new ResponseEntity<List<BoardDto.BoardResponseDto>>(boardResponseDtoList, HttpStatus.OK);
+    }
+
+    /** 게시글 상세 조회 */
+    @GetMapping("/lookup/{board_no}")
+    @ApiOperation(value = "게시글 상세 조회")
+    public ResponseEntity<BoardDto.BoardResponseDto> getBoardById(@PathVariable int board_no) {
+        BoardDto.BoardResponseDto boardResponseDto= boardService.findById(board_no);
+        return new ResponseEntity<BoardDto.BoardResponseDto>(boardResponseDto, HttpStatus.OK);
+    }
+
+    /** 게시글 등록 */
+    @PostMapping("/insert")
+    @ApiOperation(value = "게시글 등록")
+    public ResponseEntity<Integer> insertBoard(@RequestBody BoardDto.BoardSaveRequestDto boardSaveRequestDto) {
+        int insertedBoardNo = boardService.insertBoard(boardSaveRequestDto);
+        return new ResponseEntity<Integer>(insertedBoardNo, HttpStatus.CREATED);
+    }
+
+    /** 게시글 수정 */
+    @PutMapping("/update/{board_no}")
+    @ApiOperation(value = "게시글 수정")
+    public ResponseEntity<Integer> updateBoard(@PathVariable int board_no, @RequestBody BoardDto.BoardUpdateRequestDto boardUpdateRequestDto) {
+        int updatedBoardNo = boardService.updateBoard(board_no, boardUpdateRequestDto);
+        return new ResponseEntity<Integer>(updatedBoardNo, HttpStatus.CREATED);
+    }
+
+    /** 게시글 삭제 */
+    @PutMapping("/delete/{board_no}")
+    @ApiOperation(value = "게시글 삭제")
+    public ResponseEntity<Integer> deleteBoard(@PathVariable int board_no) {
+        int deletedBoardNo = boardService.deleteBoard(board_no);
+        return new ResponseEntity<Integer>(deletedBoardNo, HttpStatus.CREATED);
+    }
+
+    /** 게시글 복구 */
+    @PutMapping("/restore/{board_no}")
+    @ApiOperation(value = "게시글 복구")
+    public ResponseEntity<Integer> restoreBoard(@PathVariable int board_no) {
+        int restoreBoardNo = boardService.restoreBoard(board_no);
+        return new ResponseEntity<Integer>(restoreBoardNo, HttpStatus.CREATED);
+    }
 }
---
+++ backend/naite-backend/src/main/java/com/ssafy/naite/service/Board/BoardService.java
@@ -0,0 +1,69 @@
+package com.ssafy.naite.service.Board;
+
+import com.ssafy.naite.domain.board.Board;
+import com.ssafy.naite.domain.board.BoardRepository;
+import com.ssafy.naite.dto.board.BoardDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    /** 게시글 전체 조회 */
+    @Transactional(readOnly = true)
+    public List<BoardDto.BoardResponseDto> findAllBoards() {
+        return boardRepository.findAll().stream().filter(board->board.getBoard_is_deleted() == 0).map(BoardDto.BoardResponseDto::new).collect(Collectors.toList());
+    }
+
+    /** 게시글 상세 조회 */
+    @Transactional(readOnly = true)
+    public BoardDto.BoardResponseDto findById(int board_no) {
+        Board board = boardRepository.findById(board_no)
+                                    .orElseThrow(()-> new IllegalAccessError("[board_no=" + board_no + "] 해당 게시글이 존재하지 않습니다."));
+
+        return new BoardDto.BoardResponseDto(board);
+    }
+
+    /** 게시글 등록 */
+    @Transactional
+    public int insertBoard(BoardDto.BoardSaveRequestDto boardSaveRequestDto) {
+        return boardRepository.save(boardSaveRequestDto.toEntity()).getBoard_no();
+    }
+
+    /** 게시글 수정 */
+    @Transactional
+    public int updateBoard(int board_no, BoardDto.BoardUpdateRequestDto boardUpdateRequestDto) {
+        Board board = boardRepository.findById(board_no)
+                                    .orElseThrow(()-> new IllegalAccessError("[board_no=" + board_no + "] 해당 게시글이 존재하지 않습니다."));
+        board.update(boardUpdateRequestDto.getBoard_title(),boardUpdateRequestDto.getBoard_content(),
+                boardUpdateRequestDto.getBoard_pic(), boardUpdateRequestDto.getUnknown_flag(), boardUpdateRequestDto.getOpen_flag());
+
+        return board_no;
+    }
+
+    /** 게시글 삭제 */
+    public int deleteBoard(int board_no) {
+        Board board = boardRepository.findById(board_no)
+                                    .orElseThrow(()-> new IllegalAccessError("[board_no=" + board_no + "] 해당 게시글이 존재하지 않습니다."));
+        board.delete(1);
+        boardRepository.save(board);
+
+        return board_no;
+    }
+
+    /** 게시글 복구 */
+    public int restoreBoard(int board_no) {
+        Board board = boardRepository.findById(board_no)
+                                    .orElseThrow(()-> new IllegalAccessError("[board_no=" + board_no + "] 해당 게시글이 존재하지 않습니다."));
+        board.delete(0);
+        boardRepository.save(board);
+        return board_no;
+    }
+}
