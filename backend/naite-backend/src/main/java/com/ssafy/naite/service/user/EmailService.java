c9836bcf843f28f427a96675b9c3e36957af7a09
조성훈 <sung95419@gmail.com>
2021-01-28 11:08:43 +0900

    기능추가: controller에 crossorigin 추가

--- backend/naite-backend/src/main/java/com/ssafy/naite/service/user/EmailService.java
+++ backend/naite-backend/src/main/java/com/ssafy/naite/service/user/EmailService.java
@@ -1,4 +1,4 @@
-package com.ssafy.naite.service.User;
+package com.ssafy.naite.service.user;
 
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
