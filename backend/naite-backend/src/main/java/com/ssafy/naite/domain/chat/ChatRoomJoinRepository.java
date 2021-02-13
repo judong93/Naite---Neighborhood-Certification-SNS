5b2c4d4ba8905346cf8ac2dd62ec9c83952b2c3f
kimmk0924 <kimmk0924@gmail.com>
2021-02-13 17:34:37 +0900

    기능추가: 특정 사용자가 해당 채팅방에 속해 있지 않은 예외 처리

--- backend/naite-backend/src/main/java/com/ssafy/naite/domain/chat/ChatRoomJoinRepository.java
+++ backend/naite-backend/src/main/java/com/ssafy/naite/domain/chat/ChatRoomJoinRepository.java
@@ -4,7 +4,9 @@ import com.ssafy.naite.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRoomJoinRepository extends JpaRepository<ChatRoomJoin, Integer> {
     List<ChatRoomJoin> findByUser(User user);
+    Optional<ChatRoomJoin> findByUserAndChatRoom(User user, ChatRoom chatRoom);
 }
